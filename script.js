3ee62eeffc5a811789b6fe6fc656c4714d14fb13
Yamanouenookura <130156464+Yamanouenookura@users.noreply.github.com>
2024-09-02 22:48:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-window.onload = function() {
+$(document).ready(function() {
+    // イベントリストの追加
+    $('#event-list').append('<li>イベント1</li>');
+    $('#event-list').append('<li>イベント2</li>');
+
+    // ニュースリストの追加
+    $('#news-list').append('<li>ニュース1</li>');
+    $('#news-list').append('<li>ニュース2</li>');
+
+    // EmailJSのフォーム送信処理
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
+
+        // ランダムな番号を生成し、hidden inputにセット
         this.contact_number.value = Math.random() * 100000 | 0;
+
+        // EmailJSのフォーム送信
         emailjs.sendForm('service_z5cygwp', 'template_e6oc0f6', this)
             .then(function() {
                 console.log('SUCCESS!');
@@ -11,4 +24,4 @@ window.onload = function() {
                 alert('送信に失敗しました。もう一度お試しください。');
             });
     });
-}
+});
